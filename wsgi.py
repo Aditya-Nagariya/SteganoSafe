d8bddfbdc2c3e8c0d16121467cf8e8a239c4873f
Aditya-Nagariya <adityanagariyav@gmail.com>
2025-03-03 11:12:55 +0530

    fixing

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,21 @@
 import os
 import sys
+
 # Add the current directory to the Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from app import app, socketio, db, create_default_admin
 
-# Initialize database and create admin user when deployed
+# Initialize database and create admin user
 with app.app_context():
     db.create_all()
     create_default_admin()
 
+# This enables both gunicorn and direct running
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    socketio.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0", port=port)
+else:
+    # This is for gunicorn
+    # We don't need anything special here as gunicorn will use the 'app' variable
+    pass
